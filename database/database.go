1486b00800dca323c467e15d072423517bbfa1fa
Martin Kim Dung-Pham <kim@elbedev.com>
2018-02-10 20:56:55 +0100

    Add ALETHEA_POSTGRESQL_ADDRESS env var

--- database/database.go
+++ database/database.go
@@ -13,7 +13,9 @@ func PostgresqlDatabase() pg.DB {
 	user := os.Getenv("ALETHEA_POSTGRESQL_USER")
 	password := os.Getenv("ALETHEA_POSTGRESQL_PASSWORD")
 	database := os.Getenv("ALETHEA_POSTGRESQL_DATABASE")
+	address := os.Getenv("ALETHEA_POSTGRESQL_ADDRESS")
 	return *pg.Connect(&pg.Options{
+		Addr: address,
 		User:     user,
 		Password: password,
 		Database: database,
