4d62cc37e5ffef8efa237fbe537d2f2fb7372f20
Martin Kim Dung-Pham <kim.dung-pham@xing.com>
2018-02-24 19:38:05 +0100

    Parse port from command line arguments

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"os"
 
 	"github.com/apex/log"
@@ -8,6 +9,8 @@ import (
 	"github.com/q231950/alethea/datastorage"
 	"github.com/q231950/alethea/server"
 )
+
+const defaultPort = 8080
 
 func main() {
 	log.SetHandler(cli.New(os.Stderr))
@@ -18,6 +21,9 @@ func main() {
 	dataStorage := datastorage.New()
 	dataStorage.CreateIncidentsTable()
 
-	server := server.NewServer(dataStorage)
+	p := flag.Int("port", defaultPort, "help message for flagname")
+	flag.Parse()
+
+	server := server.NewServer(dataStorage, *p)
 	server.Serve()
 }
